988660a64bf26c7efa9351d83c4093f4028cf5c0
vladgray777 <148011516+vladgray777@users.noreply.github.com>
2025-12-26 13:25:56 -0500

    added test6

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -46,7 +46,7 @@ function App() {
 			<div className="card">
 				<button
 					onClick={() => {
-						fetch("/api/")
+						fetch("/api/test")
 							.then((res) => res.json() as Promise<{ name: string }>)
 							.then((data) => setName(data.name));
 					}}
