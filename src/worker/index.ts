4102a7e393ec2c471ed7fc87791e2647ed514242
vladgray777 <148011516+vladgray777@users.noreply.github.com>
2025-12-26 13:28:45 -0500

    push test

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
-
+app.get("/api/test", (c) => c.json({ name: "KCD Software" }));
 export default app;
 
 
