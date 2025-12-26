189b5c0ebc9b2c8ad3418560bcec8567221bce74
vladgray777 <148011516+vladgray777@users.noreply.github.com>
2025-12-26 12:36:22 -0500

    test3

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -31,7 +31,7 @@ function App() {
 					/>
 				</a>
 			</div>
-			<h1>Vite + React + Hono + Cloudflare + 777</h1>
+			<h1>Vite + React + Hono + Cloudflare + 77788</h1>
 			<div className="card">
 				<button
 					onClick={() => setCount((count) => count + 1)}
--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,31 @@
+// import { Hono } from "hono";
+// const app = new Hono<{ Bindings: Env }>();
+
+// app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+
+// export default app;
+
+
 import { Hono } from "hono";
+
+export interface Env {
+  KV: KVNamespace;
+}
+
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+app.get("/api/", (c) => {
+  return c.json({ name: "Cloudflare" });
+});
+
+app.get("/api/questions", async (c) => {
+  const questions = await c.env.KV.get("n400", { type: "json" });
+
+  if (!questions) {
+    return c.json({ error: "Questions not found" }, 404);
+  }
+
+  return c.json(questions);
+});
 
 export default app;
